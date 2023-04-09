b6983a1738375d5bd142e1d13b28a2591935cde5
RAMMALAKSHMI <125110690+RAMMALAKSHMI@users.noreply.github.com>
2023-04-09 12:54:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function displaydetails(){
+    var resfname = document.getElementById("fname").value;
+    console.log("FIRST NAME :" + resfname);
+
+    var resmname = document.getElementById("mname").value;
+    console.log("MIDDLE NAME :" + resmname);
+
+    var reslname = document.getElementById("lname").value;
+    console.log("LAST NAME :" + reslname);
+
+    var resemail = document.getElementById("email").value;
+    console.log("MAIL ID :" + resemail);
+}
